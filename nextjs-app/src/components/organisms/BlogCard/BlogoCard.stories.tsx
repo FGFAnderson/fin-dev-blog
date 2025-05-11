432b77723b384422909b00f644262dca77ae368f
fgfanderson <fgfanderson@gmail.com>
2025-05-11 04:59:30 +0000

    refactor: Moved Header and BlogCard into organisms

---
+++ nextjs-app/src/components/organisms/BlogCard/BlogoCard.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import BlogCard from "./BlogCard";
+
+const meta: Meta<typeof BlogCard> = {
+  title: "Components/molecules/BlogCard",
+  component: BlogCard,
+  tags: ["autodocs"],
+  argTypes: {
+    description: {
+      control: { type: "text" },
+    },
+    datePublished: {
+      control: { type: "date" },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof BlogCard>;
+
+export const Default: Story = {
+  args: {
+    image:
+      "https://media.istockphoto.com/id/473035236/vector/object-oriented-programming-oop.jpg?s=612x612&w=0&k=20&c=2vBUhGzzaesGJWpKtVMqA-aOPHskDIIizSdtMwZOy9Q=",
+    width: 400,
+    linkToPost: "/",
+    title: "The consequences of deep inheritance",
+    description:
+      "Deep inheritance ravages the readability of your codebase, here's 5 ways you can fix it",
+    datePublished: new Date("2025-04-22"),
+  },
+};
---
+++ nextjs-app/src/components/organisms/Header/Header.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import BrandLogo from "@components/atoms/BrandLogo";
+import Toolbar from "@mui/material/Toolbar";
+import NavItemList from "@components/molecules/NavItemList/NavItemList";
+import SearchField from "@components/atoms/SearchField";
+import Box from "@mui/material/Box";
+
+export default function Header() {
+  const navItems = [
+    { href: "/", label: "Home" },
+    { href: "/about", label: "About" },
+    { href: "/contact", label: "Contact" },
+  ];
+
+  return (
+    <AppBar color="transparent" position="static">
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <BrandLogo width={120} height="auto" />
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            flexGrow: 1,
+          }}
+        >
+          <NavItemList items={navItems} />
+        </Box>
+
+        <Box>
+          <SearchField width={240} variant="standard" placeholder="Search..." />
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
